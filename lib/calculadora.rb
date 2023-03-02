3b27b40fd9997df1c96505a9164cb996bf85bb1c
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-02 20:50:19 -0300

    Concluindo desafio 1

--- lib/calculadora.rb
+++ lib/calculadora.rb
@@ -1,8 +1,47 @@
 def soma(primeiro_numero, segundo_numero)
+    primeiro_numero + segundo_numero
 end
 
 def subtracao(primeiro_numero, segundo_numero)
+    primeiro_numero - segundo_numero
 end
 
-def multiplicacao
+def multiplicacao(primeiro_numero, segundo_numero)
+    primeiro_numero * segundo_numero
 end
+
+def divisao(primeiro_numero, segundo_numero)
+    segundo_numero == 0 ? 'Opa! Zero como divisor' : primeiro_numero / segundo_numero
+end
+
+def radiciacao(qual_valor_quer_tirar_raiz, indice_da_raiz)
+    if qual_valor_quer_tirar_raiz == 0
+        0
+    elsif qual_valor_quer_tirar_raiz >= 1
+        (qual_valor_quer_tirar_raiz**(1.0/indice_da_raiz)).round
+    else
+        "Não é possível extrair raiz quadrada de um número negativo no conjunto dos números reais."
+    end
+end
+
+def potenciacao(qual_valor_quer_elevar, expoente)
+    if qual_valor_quer_elevar == 0
+        0
+    elsif expoente == 0
+        1
+    else
+        if (expoente > 0)
+            (qual_valor_quer_elevar**expoente)
+        else
+            resultado = (1.0/(qual_valor_quer_elevar**(-1*expoente)))
+            if qual_valor_quer_elevar.kind_of?Float
+                resultado.round 
+            else
+                resultado
+            end
+        end
+    end
+end
+
+p(potenciacao(0.2, -5))
+
--- spec/calculadora_spec.rb
+++ spec/calculadora_spec.rb
@@ -33,4 +33,36 @@ describe 'Calculadora' do
   it '8: exibe mensagem ao dividir por zero' do
     expect(divisao(10, 0)).to eq('Opa! Zero como divisor')
   end
+
+  it '9: deve tirar raiz do numero corretamente' do
+    expect(radiciacao(59049, 5)).to eq(9)
+  end
+
+  it '10: deve tirar raiz de 0 corretamente' do
+    expect(radiciacao(0, 999)).to eq(0)
+  end
+
+  it '11: deve exibir mensagem de erro ao tirar raiz de numero negativo' do
+    expect(radiciacao(-59049, 5)).to eq('Não é possível extrair raiz quadrada de um número negativo no conjunto dos números reais.')
+  end
+
+  it '12: deve elevar o numero corretamente' do
+    expect(potenciacao(9, 5)).to eq(59049)
+  end
+
+  it '13: deve elevar o 0 corretamente' do
+    expect(potenciacao(0, 999)).to eq(0)
+  end
+
+  it '14: deve elevar numero negativo corretamente' do
+    expect(potenciacao(-9, 5)).to eq(-59049)
+  end
+
+  it '15: deve elevar numero decimal com expoente negativo corretamente' do
+    expect(potenciacao(0.2, -5)).to eq(3125)
+  end
+
+  it '14: deve elevar numero decimal negativo com expoente negativo corretamente' do
+    expect(potenciacao(-0.2, -5)).to eq(-3125)
+  end
 end

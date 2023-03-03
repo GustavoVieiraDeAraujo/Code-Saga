4791c950619f999ccd27a2f7c975c0ec9ebc6bf1
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-02 23:38:16 -0300

    Concluindo desafio 7

--- lib/array_utils.rb
+++ lib/array_utils.rb
@@ -1,5 +1,17 @@
 class ArrayUtils
   def self.multiplos(qtd, multiplo)
+    retorno = []
+    (1..qtd).step(1) do |n|
+      retorno.push(multiplo*n)
+    end
+    retorno
+  end
 
+  def self.tabuada(numero)
+    retorno = []
+    (1..numero).step(1) do |n|
+      retorno.push(multiplos(10, n))
+    end
+    retorno
   end
 end

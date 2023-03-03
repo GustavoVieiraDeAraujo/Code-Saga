4484f215c068a8c05c74436870408a9863d1894f
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-02 22:35:54 -0300

    Concluindo desafio 3

--- lib/string_utils.rb
+++ lib/string_utils.rb
@@ -1,4 +1,16 @@
 class StringUtils
   def wavefy(string)
+    retorno = ''
+    flag = true
+    string.chars.each do |letter|
+      if flag
+        retorno += letter.downcase
+        flag = false
+      else
+        retorno += letter.upcase
+        flag = true
+      end
+    end
+    retorno
   end
 end

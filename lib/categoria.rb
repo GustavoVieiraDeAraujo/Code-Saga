00bc7e8221f1ea2f0cd3c98e78cd889523bc1522
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-03 10:17:22 -0300

    Concluindo desafio 11

--- lib/categoria.rb
+++ lib/categoria.rb
@@ -1,6 +1,12 @@
 class Categoria
   attr_accessor :nome
-  def initialize(nome: 'Geral')
+  def initialize(nome = 'Geral')
     @nome = nome
   end
 end
+
+
+teste =  Categoria.new('Teste')
+p (teste)
+teste =  Categoria.new()
+p (teste)

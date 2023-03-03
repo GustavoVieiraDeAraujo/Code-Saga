9c486b65e34e9fbd24e88eb47e51460a8fd3f051
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-02 23:22:34 -0300

    Concluindo desafio 6

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -1,7 +1,11 @@
 class Arrays
   def self.converte_impares_por(lista, numero)
+    impares = lista.select{|numero| numero % 2 != 0}
+    [impares, impares.map{|impar| impar*numero}]
   end
 
   def self.converte_pares_por(lista, numero)
+    pares = lista.select{|numero| numero % 2 == 0}
+    [pares, pares.map{|par| par*numero}]
   end
 end

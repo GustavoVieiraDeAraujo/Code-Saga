eccc7ae3ad7d8cebd300a5496a7279ccb221be48
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-03 09:22:58 -0300

    Concluindo desafio 10

--- lib/chars.rb
+++ lib/chars.rb
@@ -1,4 +1,16 @@
 class Chars
   def unique_quantity(text)
+    contador = 0
+    text = text.split(' ')
+    text.each do |sequence|
+      temp = []
+      sequence.chars.each do |letter| 
+        if !(temp.include? letter)
+          contador += 1
+          temp = [*temp, letter]
+        end
+      end
+    end
+    contador
   end
 end

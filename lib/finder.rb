680234a5f9b513ecd8cbd31a51181b9b1cc91869
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-02 23:10:52 -0300

    Concluindo desafio 5

--- lib/finder.rb
+++ lib/finder.rb
@@ -1,4 +1,13 @@
 class Finder
   def index_of(list, value)
+    retorno = []
+    list.each do |element|
+      if element.include? value
+        posicao = list.find_index(element) 
+        retorno.push(posicao)
+        list[posicao] = nil
+      end      
+    end
+    retorno
   end
 end

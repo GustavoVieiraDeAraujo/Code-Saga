4c8fdd8a1c0ac57c7c3cc0fea2199388d700f685
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-03 00:07:20 -0300

    Concluindo desafio 8

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -1,4 +1,11 @@
 class Arrays
   def self.multiplica_antecessor_predecessor(array)
+    retorno = [array[0]*array[1]]
+    tamanho_array = array.length()
+    (1..(tamanho_array-2)).step(1) do |n|
+      retorno.push(array[n-1]*array[n+1])
+    end
+    retorno.push(array[tamanho_array-2]*array[tamanho_array-1])
+    retorno
   end
 end

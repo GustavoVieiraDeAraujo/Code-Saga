9b6867daed857d8b514c2cbe6a7ad7fdfc364a1d
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-02 22:12:30 -0300

    Concluindo desafio 2

--- lib/word.rb
+++ lib/word.rb
@@ -1,7 +1,11 @@
 class Word
   def vowels_count(phrase)
+    filtro_vogal = 'aeiouyáàâãéèêíïóôõöú'
+    phrase.chars.select{|letter| filtro_vogal.include? letter.downcase}.length()
   end
 
   def consonants_count(phrase)
+    filtro_consoante ='bcdfghjklmnpqrstvwyz'
+    phrase.chars.select{|letter| filtro_consoante.include? letter.downcase}.length()
   end
 end

895a363a3714278f5a67b80c64fee26eb79406af
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-12 10:47:58 -0300

    Refatorando codigo 2

--- lib/array_utils.rb
+++ lib/array_utils.rb
@@ -2,21 +2,11 @@
 
 # Classe ArrayUtils com os metodos multiplos e tabuada
 class ArrayUtils
-  # Retorna os múltiplos de um determinado número.
   def self.multiplos(qtd, multiplo)
-    retorno = []
-    (1..qtd).step(1) do |n|
-      retorno.push(multiplo * n)
-    end
-    retorno
+    (1..qtd).map { |n| multiplo * n }
   end
 
-  # Retorna a tabuada de determinados valores.
   def self.tabuada(numero)
-    retorno = []
-    (1..numero).step(1) do |n|
-      retorno.push(multiplos(10, n))
-    end
-    retorno
+    (1..numero).map { |n| Array.new(10) { |i| (i + 1) * n } }
   end
 end

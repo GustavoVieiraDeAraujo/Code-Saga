d9b73d8361ee1aad252331f26f4f835a590fd989
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-12 10:53:55 -0300

    Refatorando codigo

--- lib/calculadora.rb
+++ lib/calculadora.rb
@@ -1,15 +1,17 @@
 def soma(primeiro_numero, segundo_numero)
-    primeiro_numero + segundo_numero
+  primeiro_numero + segundo_numero
 end
 
 def subtracao(primeiro_numero, segundo_numero)
-    primeiro_numero - segundo_numero
+  primeiro_numero - segundo_numero
 end
 
 def multiplicacao(primeiro_numero, segundo_numero)
-    primeiro_numero * segundo_numero
+  primeiro_numero * segundo_numero
 end
 
 def divisao(primeiro_numero, segundo_numero)
-    segundo_numero == 0 ? 'Opa! Zero como divisor' : primeiro_numero / segundo_numero
+  return 'Opa! Zero como divisor' if segundo_numero.zero?
+
+  primeiro_numero / segundo_numero.to_f
 end

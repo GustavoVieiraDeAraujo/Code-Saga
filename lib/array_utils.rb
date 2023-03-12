9328e5de02455960f7cd50e3ce39201d289ae854
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-12 09:56:35 -0300

    Refatorando codigo

--- lib/array_utils.rb
+++ lib/array_utils.rb
@@ -1,12 +1,17 @@
+# frozen_string_literal: true
+
+# Classe ArrayUtils com os metodos multiplos e tabuada
 class ArrayUtils
+  # Retorna os múltiplos de um determinado número.
   def self.multiplos(qtd, multiplo)
     retorno = []
     (1..qtd).step(1) do |n|
-      retorno.push(multiplo*n)
+      retorno.push(multiplo * n)
     end
     retorno
   end
 
+  # Retorna a tabuada de determinados valores.
   def self.tabuada(numero)
     retorno = []
     (1..numero).step(1) do |n|

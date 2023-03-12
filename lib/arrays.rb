75c365de585591e28687a1d6aede08a11cd701db
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-12 10:00:33 -0300

    Refatorando codigo 1

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -2,11 +2,13 @@
 
 # Classe de arrays, com metodos de converter converte_impares_por e converte_pares_por
 class Arrays
+  # Retorna lista com numeros impares convertidos para multiplos de um determinado numero.
   def self.converte_impares_por(lista, numero)
     impares = lista.select(&:odd?)
     [impares, impares.map { |impar| impar * numero }]
   end
 
+  # Retorna lista com numeros pares convertidos para multiplos de um determinado numero.
   def self.converte_pares_por(lista, numero)
     pares = lista.select(&:even?)
     [pares, pares.map { |par| par * numero }]

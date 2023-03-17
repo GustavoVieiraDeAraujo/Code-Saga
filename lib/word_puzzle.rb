e6a197e8dc6f3c43dd512327b062471fae49f4bd
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-16 21:27:00 -0300

    Refatorando desafio final 1

--- lib/word_puzzle.rb
+++ lib/word_puzzle.rb
@@ -1,36 +1,23 @@
 class WordPuzzle
   def calculate_word_weight(palavra)
     peso_palavra = 0
-    alfabeto = 'abcdefghijklmnopqrstuvwxyz'.chars
-    array_letras_palavra = palavra.chars
-    array_letras_palavras_minuscula = array_letras_palavra.map{|letra| letra.downcase}
-    verificar_se_esta_em_ordem_alfabetica = array_letras_palavras_minuscula == array_letras_palavras_minuscula.sort
-    verificar_se_tem_so_uma_letra = array_letras_palavra.length() == 1
-    if !(verificar_se_esta_em_ordem_alfabetica or verificar_se_tem_so_uma_letra)
-      array_letras_palavra.each do |letra|
-        if(letra == letra.upcase)
-          peso_palavra -= (alfabeto.find_index(letra.downcase)+1)
-        else
-          peso_palavra += (alfabeto.find_index(letra)+1)
-        end
+    alfabeto = ('a'..'z').to_a
+    array_letras_palavras_minuscula = palavra.downcase.chars
+    if !array_letras_palavras_minuscula.sort.eql?(array_letras_palavras_minuscula) && palavra.length > 1
+      palavra.each_char do |letra|
+        peso_palavra += (letra.upcase == letra) ? -(alfabeto.index(letra.downcase)+1) : (alfabeto.index(letra.downcase)+1)
       end
     end
     peso_palavra
   end
   
   def phrase_weight(phrases)
-    pesos = []
-    phrases.each do |palavra_ou_frase|
-      verificar_se_frase = palavra_ou_frase.include? ' '
-      if (verificar_se_frase)
-        peso_da_frase = 0
-        palavras_frase = palavra_ou_frase.split(' ')
-        palavras_frase.each do |palavra|
-          peso_da_frase += calculate_word_weight(palavra)
-        end
-        pesos.push(peso_da_frase)
+    pesos = phrases.map do |palavra_ou_frase|
+      if palavra_ou_frase.include?(' ')
+        palavras_frase = palavra_ou_frase.split
+        palavras_frase.sum { |palavra| calculate_word_weight(palavra) }
       else
-        pesos.push(calculate_word_weight(palavra_ou_frase))
+        calculate_word_weight(palavra_ou_frase)
       end
     end
     pesos

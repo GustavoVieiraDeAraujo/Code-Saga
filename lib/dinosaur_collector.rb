4c5fd1ab96e94b4b79b98bdc7aa144128318f2fa
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-16 21:45:39 -0300

    Refatorando desafio final 4

--- lib/dinosaur_collector.rb
+++ lib/dinosaur_collector.rb
@@ -2,24 +2,19 @@ class DinosaurCollector
   def toy_collecting(toys_positions, dino_position, battery)
     brinquedos_recuperados = 0
     caminho_percorrido = [dino_position]
-    quantidade_brinquedos = toys_positions.length()
-    
-    while (quantidade_brinquedos > 0)
-      x_dino = dino_position[0]
-      y_dino = dino_position[1]
-      distancia_brinquedos = []
-      toys_positions.each do |toy|
-        x_toy = toy[0]
-        y_toy = toy[1]
-        distancia_dois_pontos = (((x_toy - x_dino)**2) + ((y_toy - y_dino)**2))**0.5
-        distancia_brinquedos.push(distancia_dois_pontos)
+    quantidade_brinquedos = toys_positions.size
+    while quantidade_brinquedos > 0
+      x_dino, y_dino = dino_position
+      distancia_brinquedos = toys_positions.map do |toy|
+        x_toy, y_toy = toy
+        Math.sqrt((x_toy - x_dino)**2 + (y_toy - y_dino)**2)
       end
-      if (battery-distancia_brinquedos.min >= 0)
-        pos_brinquedo_mais_perto = distancia_brinquedos.find_index(distancia_brinquedos.min)
+      if battery - distancia_brinquedos.min >= 0
+        pos_brinquedo_mais_perto = distancia_brinquedos.index(distancia_brinquedos.min)
         dino_position = toys_positions[pos_brinquedo_mais_perto]
         caminho_percorrido.push(dino_position)
         toys_positions.delete_at(pos_brinquedo_mais_perto)
-
+  
         quantidade_brinquedos -= 1
         brinquedos_recuperados += 1
         battery -= distancia_brinquedos.min

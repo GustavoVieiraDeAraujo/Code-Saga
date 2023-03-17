dbfb2e3e084d810733aef23187b563cecb71ec52
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2023-03-16 21:59:35 -0300

    Refatorando desafio final 6

--- lib/naval_battle.rb
+++ lib/naval_battle.rb
@@ -5,131 +5,87 @@ class NavalBattle
     @player_b = player_b
     @matrix_size = matrix_size
   end
-
   def create_matrix(matrix_size)
-    matrix_size = matrix_size.split('x')
-    eixo_x = matrix_size[0].to_i
-    eixo_y = matrix_size[1].to_i
-
-    matriz = []
-    (1..eixo_y).step(1) do |y|
-      matriz.push(Array.new(eixo_x){|x| x = '0'})
-    end
-    matriz
+    eixo_x, eixo_y = matrix_size.split('x').map(&:to_i)
+    Array.new(eixo_y) { Array.new(eixo_x, '0') }
   end
-
   def positions_ships(player_matrix, ships_position)
+    symbol_map = {
+      boias: 'B',
+      corveta: 'c',
+      cruzador: 'C',
+      submarino: 'S',
+      porta_avioes: 'P',
+      navio_de_guerra: 'N'
+    }
     ships_position.each do |chave, valor|
-      case chave 
-      when :boias
-        valor.each do |coordenada|
-          player_matrix[coordenada[0]][coordenada[1]] = 'B'
-        end
-      when :corveta
-        valor.each do |coordenada|
-          player_matrix[coordenada[0]][coordenada[1]] = 'c'
-        end
-      when :cruzador
-        valor.each do |coordenada|
-          player_matrix[coordenada[0]][coordenada[1]] = 'C'
-        end
-      when :submarino
-        valor.each do |coordenada|
-          player_matrix[coordenada[0]][coordenada[1]] = 'S'
-        end
-      when :porta_avioes
-        valor.each do |coordenada|
-          player_matrix[coordenada[0]][coordenada[1]] = 'P'
-        end
-      when :navio_de_guerra
-        valor.each do |coordenada|
-          player_matrix[coordenada[0]][coordenada[1]] = 'N'
-        end
+      symbol = symbol_map[chave]
+      valor.each do |coordenada|
+        player_matrix[coordenada[0]][coordenada[1]] = symbol
       end
     end
     player_matrix
   end
-
   def attack(position, player_matrix)
-    embarcacao = player_matrix[position[0]][position[1]] 
+    symbol_values = {
+      '0' => 0,
+      'B' => -3,
+      'c' => 5,
+      'C' => 3,
+      'S' => 3,
+      'P' => 1,
+      'N' => 2
+    }
+    symbol = player_matrix[position[0]][position[1]]
     player_matrix[position[0]][position[1]] = 'X'
-    case embarcacao 
-    when '0'
-      0
-    when 'B'
-      -3
-    when 'c'
-      5
-    when 'C'
-      3
-    when 'S'
-      3
-    when 'P'
-      1
-    when 'N'
-      2
-    end
+    symbol_values[symbol] || 0
   end
-
   def sunken_ships(endgame_matrix, ships_position)
-    pontuacao_por_embarcacao_naufragada = 0
-    ships_position.values.each do |valor|
-      valor.each do |posicao|
-        if(endgame_matrix[posicao[0]][posicao[1]] == 'X')
-          index = valor.find_index(posicao)
-          valor[index] = 'X'
-        end
+    destroyed_ships_score = 0
+    ships_position.each do |ship_type, positions|
+      next if ship_type == :boias
+      if positions.all? { |pos| endgame_matrix[pos[0]][pos[1]] == 'X' }
+        destroyed_ships_score += 3
+      else
+        positions.reject! { |pos| endgame_matrix[pos[0]][pos[1]] == 'X' }
       end
     end
-    ships_position.each do |chave, valor|
-      if chave != :boias
-        while valor.include? "X"
-          valor.delete("X")
-        end
-        if(valor.empty?)
-          pontuacao_por_embarcacao_naufragada += 3
-        end
-      end
-    end
-    pontuacao_por_embarcacao_naufragada
+    destroyed_ships_score
   end
-
   def execute_attacks(player_matrix_a, player_matrix_b, attacks)
     pontos_a = 0
     pontos_b = 0
-
     navios_a = @player_a
     navios_b = @player_b
-    
-    attacks = attacks.split("\n")
-    attacks.each do |ataque|
-      ataque = ataque.split(" -> ")
-      quem_ataca = ataque[0]
-      posicao_atacada = ataque[1].sub('[', '').sub(']', '').split(",").map{ |x| x.to_i}
-      if( quem_ataca == 'A')
+    attacks.each_line do |ataque|
+      quem_ataca, posicao_atacada = ataque.split(" -> ")
+      posicao_atacada = posicao_atacada.delete('[]').split(',').map(&:to_i)
+      if quem_ataca == 'A'
         pontos_a += attack(posicao_atacada, player_matrix_b)
       else
         pontos_b += attack(posicao_atacada, player_matrix_a)
       end
     end
-
     pontos_a += sunken_ships(player_matrix_b, @player_b)
     pontos_b += sunken_ships(player_matrix_a, @player_a)
-  
-    if (pontos_a > pontos_b)
+    if pontos_a > pontos_b
       ["Jogador A", pontos_a, pontos_b, player_matrix_b]
     else 
       ["Jogador B", pontos_a, pontos_b, player_matrix_a]
     end
   end
-
-  def result()
+  def result
     matriz_a = create_matrix(@matrix_size)
     matriz_b = create_matrix(@matrix_size)
-    matriz_a = positions_ships(matriz_a, @player_a)
-    matriz_b = positions_ships(matriz_b, @player_b)
-    resultado = execute_attacks(matriz_a, matriz_b, @attacks)
-    {vencedor: resultado[0] , pontos_a: resultado[1] , pontos_b: resultado[2] , matriz: resultado[3]}
+    positions_ships(matriz_a, @player_a)
+    positions_ships(matriz_b, @player_b)
+    vencedor, pontos_a, pontos_b, matriz = execute_attacks(matriz_a, matriz_b, @attacks)
+    {
+      vencedor: vencedor,
+      pontos_a: pontos_a,
+      pontos_b: pontos_b,
+      matriz: matriz
+    }
   end
 end
 
